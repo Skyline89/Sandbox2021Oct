5f1c73ff7659b5b8d5c5c30a0d44d8b05f05f15f
Oybek Beknazarov <69245833+Doctoroybek0808@users.noreply.github.com>
2021-10-12 19:48:04 +0500

    Update Class1.cs

--- BusinessLayer/Class1.cs
+++ BusinessLayer/Class1.cs
@@ -1,12 +1,6 @@
-﻿using System;
-
-namespace Sandbox2021Oct.BusinessLayer
+﻿namespace Sandbox2021Oct.BusinessLayer
 {
     public class Class1
     {
-        public void Print()
-        {
-            Console.WriteLine("Print Test");
-        }
     }
 }
